b307dc11882ed0251c2fac3aa4c41ad56445e4eb
1mod2000mod1-jpg <1mod2000mod1@gmail.com>
2025-10-10 05:38:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # متغيرات البيئة من Render
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
-ADMIN_ID = int(os.environ.get('ADMIN_ID', ''))
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '8071531346:AAG6gePsfFBTXinak1XaBUIUV6glVck1KUk')
+ADMIN_ID = int(os.environ.get('ADMIN_ID', '6521966233'))
 RENDER_URL = os.environ.get('RENDER_EXTERNAL_URL', '')
 SUPPORT_USER = '@xtt19x'  # يوزر الدعم
 
